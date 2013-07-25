d32fda5df3c900a2581dd067cdd9fa8d58effe41
Wade Burelbach <Wade.Burelbach@gmail.com>
2013-07-25 00:55:55 -0400

    Started GMG Playfire; Metacritic not currently working

--- index.php
+++ index.php
@@ -1,27 +1,31 @@
-<?php //display area for input, w/ text area
+<?php //includes
+
+include 'lib/simple_html_dom.php';
+
+//display area for input, w/ text area
 ?>
 <html>
 	<body>
-		<form method='post' maxlength="150">
+		<form method="post" maxlength="150">
 			<input type="text" placeholder="URL here" name="URL" onfocus="javascript:if(this.placeholder=='URL here'){this.placeholder='';}" onblur="javascript:if(this.placeholder==''){this.placeholder='URL here';}" />
 			<input type="submit" value="Convert to Reddit table format" name="submit" />
 	</form>
 <?php //get URL on "SUBMIT" button
 	if(isset($_POST['URL']) && !empty($_POST['URL'])) {
 		$url = $_POST['URL'];
-		print('$url: '.$url."<br>");
+		print('$url: '.$url.'<br>');
 		validate_url($url);
-		print_r(parse_url($url)); print "<br>";
+		print_r(parse_url($url)); print '<br>';
 		get_schema($url);
 	}
 ?>
 <?php //validate URL
 	function validate_url($url) {
 		if(!filter_var($url, FILTER_VALIDATE_URL)) {
-			exit("Error 400: Bad Request. (Please enter a valid URL.)");
+			exit('Error 400: Bad Request. (Please enter a valid URL.)');
 		}
 		else {
-			print("URL is valid, continuing...<br>");
+			print('URL is valid, continuing...<br>');
 		}
 	}
 ?>
@@ -29,17 +33,45 @@
 <?php //function checks URL schema (NOTE: disable for initial testing purposes)
 	function get_schema($url) {
 		$url_parsed = parse_url($url);
-		switch(strtolower($url_parsed["host"])) {
+		switch(strtolower($url_parsed['host'])) {
 			case "blog.playfire.com":
-				echo "playfire!";
+				playfire($url);
 		}
 	}
- /* if domain is Playfire.com
- 
+?>
+<?php //domain is Playfire.com
+	function playfire($url) {
+		// function gets data from: div.columns-fauxcolumns div.columns-inner div.column-center-outer div.column-center-inner div#main.main.section div.widget.Blog div.blog-posts.hfeed div.date-outer div.date-posts div.post-outer div.post-hentry div.post-body.entry-content ul
+		$html = file_get_html('gmg.html');
+		foreach($html->find('div.post-body ul li a') as $link) {
+			$game['name'] = $link->plaintext;
+			metacritic($game['name']);
+		}
+	}
+?>
+<?php /*Metacritic function: checks metacritic score
+  * if no reviews: return "N/A"
+  * if less than 4 critic reviews: return each
+  * if more than 4 critic reviews: return overall
+  * return overall user score
+  *//*
+	function metacritic($game) {
+		$game_mc = urlencode($game);
+		$html = file_get_html('http://www.metacritic.com/search/game/'.$game_mc.'/results');
+			foreach($html->find('li.result') as $result) {
+				//echo $result;
+				foreach($result->find('span[class=platform]') as $span) {
+					if($span->innertext == "PC") {
+						echo $span->parent->parent;
+					}
+				}
+				break;
+			}
+	}*/
+?>
 
-  * function gets data from: div.columns-fauxcolumns div.columns-inner div.column-center-outer div.column-center-inner div#main.main.section div.widget.Blog div.blog-posts.hfeed div.date-outer div.date-posts div.post-outer div.post-hentry div.post-body.entry-content ul
-  * (NOTE: disable for initial testing purposes)
-  *
+<?php 
+ /*
   * function then does a foreach li in ul[]; this is each game
 
   * function parses for: a->href; a->text
